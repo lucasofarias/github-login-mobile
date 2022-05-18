2928fe3e41ac51099f55fd4c3c5d419fd7591b94
Lucas <lucas.fariasag@gmail.com>
2022-05-18 11:44:52 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import {View, Text, Image, StyleSheet, SafeAreaView, StatusBar, Pressable, Linking} from 'react-native';
+
+// View = é uma caixa, basicamente uma div
+// Text = texto
+// SafeAreaView = protege o seu componente de sair para uma área que ele não tem acesso. Ex.: notch de celulares.
+
+const colorGitHub = '#010409';
+const colorFontGitHub = '#C9D1D9';
+const colorDarkFontGitHub = '#4F565E';
+
+const imageProfileGitHub = 'https://avatars.githubusercontent.com/u/90432297?v=4';
+const urlToMyGitHub = 'https://github.com/lucasofarias';
+
+const App = () => {
+    const handlePressGoToGitHub = async () => {
+        const response = await Linking.canOpenURL(urlToMyGitHub);
+        if(response) {
+            await Linking.openURL(urlToMyGitHub);
+        }
+    }
+    
+    return (
+        <SafeAreaView style={style.container}>
+            <StatusBar backgroundColor={colorGitHub} barStyle='light-content'/>
+            <View style={style.content}> 
+                <Image style={style.avatar} source={{uri: imageProfileGitHub}} accessibilityLabel='Selfie do Lucas'/>
+                <Text accessibilityLabel='Nome: Lucas Farias' style={[style.defaultText, style.name]}>Lucas Farias</Text>
+                <Text accessibilityLabel='Nickname: lucasofarias' style={[style.defaultText, style.nickname]}>lucasofarias</Text>
+                <Text accessibilityLabel='Descrição: 18 anos, cursando Ensino Superior em ADS' style={[style.defaultText, style.description]}>18 anos, cursando Ensino Superior em Análise e Desenvolvimento de Sistemas.</Text>
+                
+                <Pressable onPress={handlePressGoToGitHub}>
+                    <View style={style.button}>
+                        <Text style={[style.defaultText, style.textButton]}>Open in GitHub</Text>
+                    </View>
+                </Pressable>
+            </View>
+        </SafeAreaView>
+    );
+    
+};
+
+export default App;
+
+const style = StyleSheet.create({
+    container: { // estilo do View
+        backgroundColor: colorGitHub,
+        flex: 1, // expandir para a tela inteira.
+        justifyContent: 'center',
+    },
+
+    content: {
+        /*
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        */
+        alignItems: 'center',
+        padding: 20,
+    },
+
+    avatar: {
+        height: 200,
+        width: 200,
+        borderRadius: 100,
+        borderColor: 'white',
+        borderWidth: 2,
+    },
+
+    defaultText: {
+        color: colorFontGitHub,
+    },
+
+    name: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 20,
+    },
+
+    nickname: {
+        fontSize: 18,
+        color: colorDarkFontGitHub,
+    },
+
+    description: {
+        fontSize: 14,
+        fontWeight: 'bold',
+        marginTop: 10,
+    },
+
+    button: {
+        backgroundColor: colorDarkFontGitHub,
+        borderRadius: 10,
+        padding: 20,
+        marginTop: 20,
+    },
+
+    textButton: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+});
